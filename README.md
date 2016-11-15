77a41a59d1902da6290c396fa764f2f4516be092
uxin603s <uxin603s@gmail.com>
2016-11-15 17:57:01 +0800

    大改

--- README.md
+++ README.md
@@ -1,13 +1,1 @@
-master
-```
-var pack={
-  source:iframe.contentWindow,
-  connect:'tagSystem',
-}
-postMessageHelper.master(pack,function(res){})
-```
-------------------------
-slave
-```
-postMessageHelper.slave('tagSystem',value)
-```
+請參照index.php檔
--- postMessageHelper.js
+++ postMessageHelper.js
@@ -1,79 +1,62 @@
 var postMessageHelper={
 	connect:{},
-	masterTimer:{},
-	slaveTimer:{},
-	listen:{},
-	master:function(pack,callback){
+	sendTimer:{},
+	init:function(connect,post_window,callback){
 		var self=this;
-		if(pack.token){
-			var status=2;
-		}else{
-			pack.token=Date.now();
-			var status=0;
+		self.connect[connect]={
+			post_window:post_window,
+			status:0,//1代表連線
+		};
+	},
+	send:function(connect,sendData){
+		var self=this;
+		self.connect[connect] || (self.connect[connect]={});
+		if(!self.connect[connect].post_window){
+			clearTimeout(self.sendTimer[JSON.stringify(connect)+JSON.stringify(sendData)])
+			self.sendTimer[JSON.stringify(connect)+JSON.stringify(sendData)]=setTimeout(function(){
+				self.send(connect,sendData);
+			},500)
+			return;
 		}
 		
 		var send={
-			sendData:pack.sendData,
-			connect:pack.connect,
-			token:pack.token,
-			status:status,
+			sendData:sendData,
+			connect:connect,
+			status:self.connect[connect].status,
 		}
-		
-		if(status==0){
-			self.masterTimer[pack.connect]=setInterval(function(){
-				pack.source.postMessage(send,"*");
+		if(self.connect[connect].status){
+			self.connect[connect].post_window.postMessage(send,"*");
+		}else {
+			var timer=setInterval(function(){
+				self.connect[connect].post_window.postMessage(send,"*");
 			},500)
-			// console.log(self.masterTimer[pack.connect])
-		}else 
-		if(status==2){
-			pack.source.postMessage(send,"*");
-		}
-		
-		window.addEventListener("message",function(e){
-			// console.log(pack.connect,e.data)
-			if(e.data.connect!=pack.connect)return;
-			if(e.data.token!=pack.token)return;
-			if(e.data.status==1){
-				// console.log(self.masterTimer[pack.connect])
-				clearTimeout(self.masterTimer[pack.connect]);
-			}
-			if(e.data.status==2){
-				callback && callback(e.data.sendData)
-			}
-		},false)
-	},
-	slave:function(connect,sendData){
-		var self=this;
-		if(!self.listen[connect]){
-			self.listen[connect]=1;
-			
 			window.addEventListener("message",function(e){
-				if(e.data.status==0 && e.data.connect==connect){
-					var send={
-						status:1,
-						connect:connect,
-						token:e.data.token,
-					}
-					e.source.postMessage(send,"*");
-					var pack={
-						source:e.source,
-						token:e.data.token,
-						connect:connect,
-					}
-					self.connect[connect]=pack;
-					self.slave(connect,sendData)
+				if(e.data.connect!=connect)return;
+				if(e.data.status==2){
+					clearTimeout(timer);
+					self.connect[connect].status=1;
 				}
 			},false);
-			
-		}else if(self.connect[connect]){
-			clearTimeout(self.slaveTimer[connect]);
-			self.slaveTimer[connect]=setTimeout(function(){
-				var pack=self.connect[connect];
-				pack.sendData=sendData;
-				self.master(pack)
-			},50)
 		}
 		
+	},
+	receive:function(connect,callback){
+		var self=this;
+		self.connect[connect] || (self.connect[connect]={});
+		window.addEventListener("message",function(e){
+			if(e.data.connect!=connect)return;
+			if(e.data.status==2)return;
+			if(e.data.status==0){
+				var send={
+					status:2,
+					connect:connect,
+				}
+				e.source.postMessage(send,"*");
+				self.connect[connect].post_window=e.source;
+				self.connect[connect].status=1;
+			}
+			callback && callback(e.data.sendData);
+		},false);
 	},
 }
 
