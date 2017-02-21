03add81638d8cae7e47e46dab8f763231cf36c97
uxin603s <uxin603s@gmail.com>
2017-02-21 12:28:30 +0800

    update

--- app/components/api/api.js
+++ app/components/api/api.js
@@ -1,26 +1,21 @@
-angular.module('app').component("index",{
+angular.module('app').component("api",{
 bindings:{},
 templateUrl:'app/components/api/api.html?t='+Date.now(),
 controller:['$scope',
-function($scope){
+function($scope){	
 	var time={}
 	postMessageHelper.receive('tagSystem',function(res){
-		clearTimeout(time[res.name])
-		time[res.name]=setTimeout(function(){
-			time[res.name]=setInterval(function(){
-				clearTimeout(time[res.name])
-				if(res.name=="post"){
-					var request=res.value.request;
-					var id=res.value.id;
-					$.post("ajax.php",request,function(res){
-						postMessageHelper
-							.send("tagSystem",{name:'post',value:{id:id,value:res}})
+		if(res.name=="post"){
+			
+			var request=res.value.request;
+			var id=res.value.id;
+			$.post("ajax.php",request,function(res){
+				// console.log("b",id,res)
+				postMessageHelper
+					.send("tagSystem",{name:'post',value:{id:id,value:res}})
 
-					},"json")
-				}
-				$scope.$apply();
-			},0)
-		},0);
+			},"json")
+		}
 	});
 }],
 })
